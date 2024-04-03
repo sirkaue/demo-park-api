f3268522870e4b817895e32ce09af1067a1ce323
sirkaue <kauekdslopes@gmail.com>
2024-04-03 14:06:50 -0300

    Adicionando Entities

---
+++ src/main/java/com/sirkaue/demoparkapi/entities/Usuario.java
@@ -0,0 +1,123 @@
+package com.sirkaue.demoparkapi.entities;
+
+import com.sirkaue.demoparkapi.entities.enums.Role;
+import jakarta.persistence.*;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Entity
+@Table(name = "usuarios")
+public class Usuario implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "username", nullable = false, unique = true, length = 100)
+    private String username;
+    @Column(name = "password", nullable = false, length = 200)
+    private String password;
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role", nullable = false, length = 25)
+    private Role role;
+    @Column(name = "data_criacao")
+    private LocalDateTime dataCriacao;
+    @Column(name = "data_modificacao")
+    private LocalDateTime dataModificacao;
+    @Column(name = "criado_por")
+    private String criadoPor;
+    @Column(name = "modificado_por")
+    private String modificadoPor;
+
+    public Usuario() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public LocalDateTime getDataCriacao() {
+        return dataCriacao;
+    }
+
+    public void setDataCriacao(LocalDateTime dataCriacao) {
+        this.dataCriacao = dataCriacao;
+    }
+
+    public LocalDateTime getDataModificacao() {
+        return dataModificacao;
+    }
+
+    public void setDataModificacao(LocalDateTime dataModificacao) {
+        this.dataModificacao = dataModificacao;
+    }
+
+    public String getCriadoPor() {
+        return criadoPor;
+    }
+
+    public void setCriadoPor(String criadoPor) {
+        this.criadoPor = criadoPor;
+    }
+
+    public String getModificadoPor() {
+        return modificadoPor;
+    }
+
+    public void setModificadoPor(String modificadoPor) {
+        this.modificadoPor = modificadoPor;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Usuario usuario = (Usuario) o;
+        return Objects.equals(id, usuario.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" +
+                "id=" + id +
+                '}';
+    }
+}
---
+++ src/main/java/com/sirkaue/demoparkapi/entities/enums/Role.java
@@ -0,0 +1,6 @@
+package com.sirkaue.demoparkapi.entities.enums;
+
+public enum Role {
+    ROLE_ADMIN,
+    ROLE_CLIENTE
+}
