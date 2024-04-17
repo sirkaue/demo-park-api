43cb6d5d71b422e0cee37f0a6df2815149e9febe
sirkaue <kauekdslopes@gmail.com>
2024-04-16 21:42:53 -0300

    Lidando com DTO de resposta na consulta por id

--- src/main/java/com/sirkaue/demoparkapi/web/controllers/UsuarioController.java
+++ src/main/java/com/sirkaue/demoparkapi/web/controllers/UsuarioController.java
@@ -27,9 +27,9 @@ public class UsuarioController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Usuario> getById(@PathVariable Long id) {
+    public ResponseEntity<UsuarioResponseDto> getById(@PathVariable Long id) {
         Usuario user = usuarioService.buscarPorId(id);
-        return ResponseEntity.ok(user);
+        return ResponseEntity.ok(UsuarioMapper.toDto(user));
     }
 
     @PatchMapping("/{id}")
