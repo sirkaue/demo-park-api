3c54fa42f3ce94a2f7daaab65bf478b286affa80
sirkaue <kauekdslopes@gmail.com>
2024-07-18 12:05:28 -0300

    Update entity

--- src/main/java/com/sirkaue/demoparkapi/entity/Usuario.java
+++ src/main/java/com/sirkaue/demoparkapi/entity/Usuario.java
@@ -9,25 +9,34 @@ import java.util.Objects;
 @Entity
 @Table(name = "usuarios")
 public class Usuario implements Serializable {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Long id;
+
     @Column(name = "username", nullable = false, unique = true, length = 100)
     private String username;
+
     @Column(name = "password", nullable = false, length = 200)
     private String password;
+
     @Enumerated(EnumType.STRING)
     @Column(name = "role", nullable = false, length = 25)
     private Role role = Role.ROLE_CLIENTE;
+
     @Column(name = "data_criacao")
     private LocalDateTime dataCriacao;
+
     @Column(name = "data_modificacao")
     private LocalDateTime dataModificacao;
+
     @Column(name = "criado_por")
     private String criadoPor;
+
     @Column(name = "modificado_por")
     private String modificadoPor;
+
     public enum Role {
         ROLE_ADMIN, ROLE_CLIENTE
     }
@@ -109,7 +118,7 @@ public class Usuario implements Serializable {
 
     @Override
     public int hashCode() {
-        return Objects.hash(id);
+        return Objects.hashCode(id);
     }
 
     @Override
