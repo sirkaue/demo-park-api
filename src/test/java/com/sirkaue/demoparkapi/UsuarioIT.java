7ca86300b872f31b883b2e9b43e603bfae7f6179
sirkaue <kauekdslopes@gmail.com>
2024-07-22 16:25:09 -0300

    Camada de Testes

---
+++ src/test/java/com/sirkaue/demoparkapi/UsuarioIT.java
@@ -0,0 +1,15 @@
+package com.sirkaue.demoparkapi;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Sql(scripts = "/sql/usuarios/usuarios-insert.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+@Sql(scripts = "/sql/usuarios/usuarios-delete.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+public class UsuarioIT {
+
+    @Autowired
+    WebTestClient testClient;
+}
---
+++ src/test/resources/sql/usuarios/usuarios-insert.sql
@@ -0,0 +1,4 @@
+insert into USUARIOS (id, username, password, role) values (100, 'ana@email.com', '123456', 'ROLE_ADMIN');
+insert into USUARIOS (id, username, password, role) values (101, 'bob@email.com', '123456', 'ROLE_ADMIN');
+insert into USUARIOS (id, username, password, role) values (102, 'bia@email.com', '123456', 'ROLE_CLIENTE');
+insert into USUARIOS (id, username, password, role) values (103, 'jon@email.com', '123456', 'ROLE_CLIENTE');
