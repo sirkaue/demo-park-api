25b64d73174bf4a6227a96eac8e8c2d3028d8bc2
sirkaue <kauekdslopes@gmail.com>
2024-07-22 13:03:12 -0300

    Documentando o recurso para alterar a senha

--- src/main/java/com/sirkaue/demoparkapi/web/controller/UsuarioController.java
+++ src/main/java/com/sirkaue/demoparkapi/web/controller/UsuarioController.java
@@ -75,6 +75,26 @@ public class UsuarioController {
         return ResponseEntity.ok(UsuarioMapper.toDto(user));
     }
 
+    @Operation(summary = "Atualizar senha", description = "Atualizar senha",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "204",
+                            description = "Senha atualizada com sucesso",
+                            content = @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = Void.class))),
+
+                    @ApiResponse(
+                            responseCode = "400",
+                            description = "Senha não confere",
+                            content = @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = ErrorMessage.class))),
+
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "Recursos não encontrado",
+                            content = @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = ErrorMessage.class)))
+            })
     @PatchMapping("/{id}")
     public ResponseEntity<Void> updatePassword(@PathVariable Long id, @Valid @RequestBody UsuarioSenhaDto dto) {
         Usuario user = usuarioService.editarSenha(id, dto.getSenhaAtual(), dto.getNovaSenha(), dto.getConfirmaSenha());
