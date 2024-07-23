870e630d7be1e8e503d6dad019b26ecdceae2025
sirkaue <kauekdslopes@gmail.com>
2024-07-23 11:37:11 -0300

    JwtUserDetails

---
+++ src/main/java/com/sirkaue/demoparkapi/jwt/JwtUserDetails.java
@@ -0,0 +1,23 @@
+package com.sirkaue.demoparkapi.jwt;
+
+import com.sirkaue.demoparkapi.entity.Usuario;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
+
+public class JwtUserDetails extends User {
+
+    private Usuario usuario;
+
+    public JwtUserDetails(Usuario usuario) {
+        super(usuario.getUsername(), usuario.getPassword(), AuthorityUtils.createAuthorityList(usuario.getRole().name()));
+        this.usuario = usuario;
+    }
+
+    public Long getId() {
+        return this.usuario.getId();
+    }
+
+    public String role() {
+        return this.usuario.getRole().name();
+    }
+}
