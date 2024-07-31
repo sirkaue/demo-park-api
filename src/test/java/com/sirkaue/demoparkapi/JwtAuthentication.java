197d93d2fcd35af01a54542d6d015e29238c745f
sirkaue <kauekdslopes@gmail.com>
2024-07-31 20:30:10 -0300

    Preparando a operação de autenticação para os testes

---
+++ src/test/java/com/sirkaue/demoparkapi/JwtAuthentication.java
@@ -0,0 +1,24 @@
+package com.sirkaue.demoparkapi;
+
+import com.sirkaue.demoparkapi.jwt.JwtToken;
+import com.sirkaue.demoparkapi.web.dto.UsuarioLoginDto;
+import org.springframework.http.HttpHeaders;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import java.util.function.Consumer;
+
+public class JwtAuthentication {
+
+    public static Consumer<HttpHeaders> getHeaderAuthorization(WebTestClient client, String username, String password) {
+        String token = client
+                .post()
+                .uri("/api/v1/auth")
+                .bodyValue(new UsuarioLoginDto(username, password))
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(JwtToken.class)
+                .returnResult().getResponseBody().getToken();
+
+        return headers -> headers.add(HttpHeaders.AUTHORIZATION, "Bearer " + token);
+    }
+}
