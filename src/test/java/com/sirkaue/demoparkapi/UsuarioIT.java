b52206b3808e6a2cf62356da1a55e7ba13550ac5
sirkaue <kauekdslopes@gmail.com>
2024-08-01 18:32:08 -0300

    Método de Teste com Autenticação com/sem Permissão de Acesso

--- src/test/java/com/sirkaue/demoparkapi/UsuarioIT.java
+++ src/test/java/com/sirkaue/demoparkapi/UsuarioIT.java
@@ -357,10 +357,11 @@ public class UsuarioIT {
     }
 
     @Test
-    public void listarUsuarios_SemQualquerParametro_RetornarListaDeUsuariosComStatus200() {
+    public void listarUsuarios_ComUsuarioComPermissao_RetornarListaDeUsuariosComStatus200() {
         List<UsuarioResponseDto> responseBody = testClient
                 .get()
                 .uri("/api/v1/usuarios")
+                .headers(JwtAuthentication.getHeaderAuthorization(testClient, "ana@email.com", "123456"))
                 .exchange()
                 .expectStatus().isOk()
                 .expectBodyList(UsuarioResponseDto.class)
@@ -368,5 +369,20 @@ public class UsuarioIT {
 
         org.assertj.core.api.Assertions.assertThat(responseBody).isNotNull();
         org.assertj.core.api.Assertions.assertThat(responseBody.size()).isEqualTo(4);
+    }
+
+    @Test
+    public void listarUsuarios_ComUsuarioSemPermissao_RetornarErrorMessageComStatus403() {
+        ErrorMessage responseBody = testClient
+                .get()
+                .uri("/api/v1/usuarios")
+                .headers(JwtAuthentication.getHeaderAuthorization(testClient, "bia@email.com", "123456"))
+                .exchange()
+                .expectStatus().isForbidden()
+                .expectBody(ErrorMessage.class)
+                .returnResult().getResponseBody();
+
+        org.assertj.core.api.Assertions.assertThat(responseBody).isNotNull();
+        org.assertj.core.api.Assertions.assertThat(responseBody.getStatus()).isEqualTo(403);
     }
 }
