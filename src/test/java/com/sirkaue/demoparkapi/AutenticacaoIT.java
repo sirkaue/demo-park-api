64fdc7c150088128e8ff60f2c0629aa32e71787f
sirkaue <kauekdslopes@gmail.com>
2024-08-02 12:12:17 -0300

    Teste de autenticação com status code 200

---
+++ src/test/java/com/sirkaue/demoparkapi/AutenticacaoIT.java
@@ -0,0 +1,34 @@
+package com.sirkaue.demoparkapi;
+
+import com.sirkaue.demoparkapi.jwt.JwtToken;
+import com.sirkaue.demoparkapi.web.dto.UsuarioLoginDto;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Sql(scripts = "/sql/usuarios/usuarios-insert.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+@Sql(scripts = "/sql/usuarios/usuarios-delete.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+public class AutenticacaoIT {
+
+    @Autowired
+    WebTestClient testClient;
+
+    @Test
+    public void autenticar_ComCredenciaisValidas_RetornarTokenComStatus200() {
+        JwtToken responseBody = testClient
+                .post()
+                .uri("/api/v1/auth")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(new UsuarioLoginDto("ana@email.com", "123456"))
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(JwtToken.class)
+                .returnResult().getResponseBody();
+
+        org.assertj.core.api.Assertions.assertThat(responseBody).isNotNull();
+    }
+}
