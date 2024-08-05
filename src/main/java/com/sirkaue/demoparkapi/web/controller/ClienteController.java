b07e8c3410416445bc71d8884577c2b582171421
sirkaue <kauekdslopes@gmail.com>
2024-08-04 21:54:16 -0300

    Controle de acesso, documentando get by id

--- src/main/java/com/sirkaue/demoparkapi/web/controller/ClienteController.java
+++ src/main/java/com/sirkaue/demoparkapi/web/controller/ClienteController.java
@@ -66,7 +66,27 @@ public class ClienteController {
         return ResponseEntity.status(HttpStatus.CREATED).body(ClienteMapper.toDto(cliente));
     }
 
+    @Operation(summary = "Localizar um cliente", description = "Recurso para localizar um cliente pelo ID. " +
+            "Requisição exige uso de um Bearer Token. Acesso restrito a Role='ADMIN'",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Recurso localizado com sucesso",
+                            content = @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = ClienteResponseDto.class))),
+                    @ApiResponse(
+                            responseCode = "403",
+                            description = "Recurso não permitido ao perfil de CLIENTE",
+                            content = @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = ErrorMessage.class))),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "Cliente não encontrado.",
+                            content = @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = ErrorMessage.class)))
+            })
     @GetMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<ClienteResponseDto> getById(@PathVariable Long id) {
         Cliente cliente = clienteService.buscarPorId(id);
         return ResponseEntity.ok(ClienteMapper.toDto(cliente));
