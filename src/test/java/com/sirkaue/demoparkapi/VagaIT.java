0bc5a6a43f37785849087f91f47d805129fc285c
sirkaue <kauekdslopes@gmail.com>
2024-08-13 16:19:33 -0300

    Testando o recurso para localizar uma vaga com status 200 e 404

--- src/test/java/com/sirkaue/demoparkapi/VagaIT.java
+++ src/test/java/com/sirkaue/demoparkapi/VagaIT.java
@@ -74,4 +74,33 @@ public class VagaIT {
                 .jsonPath("method").isEqualTo("POST")
                 .jsonPath("path").isEqualTo("/api/v1/vagas");
     }
+
+    @Test
+    public void buscarVaga_ComCodigoExistente_RetornarVagaComStatus200() {
+        testClient
+                .get()
+                .uri("/api/v1/vagas/{codigo}", "A-01")
+                .headers(JwtAuthentication.getHeaderAuthorization(testClient, "ana@email.com", "123456"))
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody()
+                .jsonPath("id").isEqualTo(10)
+                .jsonPath("codigo").isEqualTo("A-01")
+                .jsonPath("status").isEqualTo("LIVRE");
+
+    }
+
+    @Test
+    public void buscarVaga_ComCodigoInexistente_RetornarErrorMessageComStatus404() {
+        testClient
+                .get()
+                .uri("/api/v1/vagas/{codigo}", "A-10")
+                .headers(JwtAuthentication.getHeaderAuthorization(testClient, "ana@email.com", "123456"))
+                .exchange()
+                .expectStatus().isNotFound()
+                .expectBody()
+                .jsonPath("status").isEqualTo(404)
+                .jsonPath("method").isEqualTo("GET")
+                .jsonPath("path").isEqualTo("/api/v1/vagas/A-10");
+    }
 }
