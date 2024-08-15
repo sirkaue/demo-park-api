bd8e99cef042126290f6f249d180d7574122a480
sirkaue <kauekdslopes@gmail.com>
2024-08-14 21:35:21 -0300

    Criando o mapeamento para a tabela Clientes_Tem_Vagas

---
+++ src/main/java/com/sirkaue/demoparkapi/entity/ClienteVaga.java
@@ -0,0 +1,239 @@
+package com.sirkaue.demoparkapi.entity;
+
+import jakarta.persistence.*;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Entity
+@Table(name = "cliente_tem_vagas")
+@EntityListeners(AuditingEntityListener.class)
+public class ClienteVaga {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "numero_recibo", nullable = false, unique = true, length = 15)
+    private String recibo;
+
+    @Column(name = "placa", nullable = false, length = 8)
+    private String placa;
+
+    @Column(name = "marca", nullable = false, length = 45)
+    private String marca;
+
+    @Column(name = "modelo", nullable = false, length = 45)
+    private String modelo;
+
+    @Column(name = "cor", nullable = false, length = 45)
+    private String cor;
+
+    @Column(name = "data_entrada", nullable = false)
+    private LocalDateTime dataEntrada;
+
+    @Column(name = "data_saida")
+    private LocalDateTime dataSaida;
+
+    @Column(name = "valor", columnDefinition = "decimal(7,2)")
+    private BigDecimal valor;
+
+    @Column(name = "desconto", columnDefinition = "decimal(7,2)")
+    private BigDecimal desconto;
+
+    @ManyToOne
+    @JoinColumn(name = "id_cliente", nullable = false)
+    private Cliente cliente;
+
+    @ManyToOne
+    @JoinColumn(name = "id_vaga", nullable = false)
+    private Vaga vaga;
+
+    @CreatedDate
+    @Column(name = "data_criacao")
+    private LocalDateTime dataCriacao;
+
+    @LastModifiedDate
+    @Column(name = "data_modificacao")
+    private LocalDateTime dataModificacao;
+
+    @CreatedBy
+    @Column(name = "criado_por")
+    private String criadoPor;
+
+    @LastModifiedBy
+    @Column(name = "modificado_por")
+    private String modificadoPor;
+
+    public ClienteVaga() {
+    }
+
+    public ClienteVaga(
+            Long id, String recibo, String placa, String marca, String modelo, String cor, LocalDateTime dataEntrada,
+            LocalDateTime dataSaida, BigDecimal valor, BigDecimal desconto, Cliente cliente, Vaga vaga,
+            LocalDateTime dataCriacao, LocalDateTime dataModificacao, String criadoPor, String modificadoPor) {
+        this.id = id;
+        this.recibo = recibo;
+        this.placa = placa;
+        this.marca = marca;
+        this.modelo = modelo;
+        this.cor = cor;
+        this.dataEntrada = dataEntrada;
+        this.dataSaida = dataSaida;
+        this.valor = valor;
+        this.desconto = desconto;
+        this.cliente = cliente;
+        this.vaga = vaga;
+        this.dataCriacao = dataCriacao;
+        this.dataModificacao = dataModificacao;
+        this.criadoPor = criadoPor;
+        this.modificadoPor = modificadoPor;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getRecibo() {
+        return recibo;
+    }
+
+    public void setRecibo(String recibo) {
+        this.recibo = recibo;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public LocalDateTime getDataEntrada() {
+        return dataEntrada;
+    }
+
+    public void setDataEntrada(LocalDateTime dataEntrada) {
+        this.dataEntrada = dataEntrada;
+    }
+
+    public LocalDateTime getDataSaida() {
+        return dataSaida;
+    }
+
+    public void setDataSaida(LocalDateTime dataSaida) {
+        this.dataSaida = dataSaida;
+    }
+
+    public BigDecimal getValor() {
+        return valor;
+    }
+
+    public void setValor(BigDecimal valor) {
+        this.valor = valor;
+    }
+
+    public BigDecimal getDesconto() {
+        return desconto;
+    }
+
+    public void setDesconto(BigDecimal desconto) {
+        this.desconto = desconto;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public Vaga getVaga() {
+        return vaga;
+    }
+
+    public void setVaga(Vaga vaga) {
+        this.vaga = vaga;
+    }
+
+    public LocalDateTime getDataCriacao() {
+        return dataCriacao;
+    }
+
+    public void setDataCriacao(LocalDateTime dataCriacao) {
+        this.dataCriacao = dataCriacao;
+    }
+
+    public LocalDateTime getDataModificacao() {
+        return dataModificacao;
+    }
+
+    public void setDataModificacao(LocalDateTime dataModificacao) {
+        this.dataModificacao = dataModificacao;
+    }
+
+    public String getCriadoPor() {
+        return criadoPor;
+    }
+
+    public void setCriadoPor(String criadoPor) {
+        this.criadoPor = criadoPor;
+    }
+
+    public String getModificadoPor() {
+        return modificadoPor;
+    }
+
+    public void setModificadoPor(String modificadoPor) {
+        this.modificadoPor = modificadoPor;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ClienteVaga that = (ClienteVaga) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
