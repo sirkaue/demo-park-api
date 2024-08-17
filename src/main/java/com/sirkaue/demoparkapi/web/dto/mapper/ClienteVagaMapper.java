d89d1444c559d55dab1e7f93d03289615f3863be
sirkaue <kauekdslopes@gmail.com>
2024-08-17 19:47:54 -0300

    Corrigindo Mapper manual

--- src/main/java/com/sirkaue/demoparkapi/web/dto/mapper/ClienteVagaMapper.java
+++ src/main/java/com/sirkaue/demoparkapi/web/dto/mapper/ClienteVagaMapper.java
@@ -1,7 +1,6 @@
 package com.sirkaue.demoparkapi.web.dto.mapper;
 
-//import com.sirkaue.demoparkapi.entity.Cliente;
-
+import com.sirkaue.demoparkapi.entity.Cliente;
 import com.sirkaue.demoparkapi.entity.ClienteVaga;
 import com.sirkaue.demoparkapi.web.dto.EstacionamentoCreateDto;
 import com.sirkaue.demoparkapi.web.dto.EstacionamentoResponseDto;
@@ -16,8 +15,8 @@ public class ClienteVagaMapper {
         return new ModelMapper().map(dto, ClienteVaga.class);
     }
 
-// Forma manual de se fazer 'mapeamento de propriedade aninhada (encadeamento)'.
-
+// // Forma manual de se fazer 'mapeamento de propriedade aninhada (encadeamento)'.
+//
 //    public static ClienteVaga toClienteVaga(EstacionamentoCreateDto dto) {
 //        ClienteVaga clienteVaga = new ClienteVaga();
 //        Cliente cliente = new Cliente();
@@ -35,4 +34,28 @@ public class ClienteVagaMapper {
     public static EstacionamentoResponseDto toDto(ClienteVaga clienteVaga) {
         return new ModelMapper().map(clienteVaga, EstacionamentoResponseDto.class);
     }
+
+
+// // Forma manual de se fazer 'mapeamento de propriedade aninhada (encadeamento)'.
+//
+//    public static EstacionamentoResponseDto toDto(ClienteVaga clienteVaga) {
+//        EstacionamentoResponseDto dto = new EstacionamentoResponseDto();
+//
+//        dto.setPlaca(clienteVaga.getPlaca());
+//        dto.setMarca(clienteVaga.getMarca());
+//        dto.setModelo(clienteVaga.getModelo());
+//        dto.setCor(clienteVaga.getCor());
+//
+//        // mapeamento de propriedade aninhada (encadeamento).
+//        dto.setClienteCpf(clienteVaga.getCliente().getCpf());
+//        dto.setRecibo(clienteVaga.getRecibo());
+//        dto.setDataEntrada(clienteVaga.getDataEntrada());
+//        dto.setDataSaida(clienteVaga.getDataSaida());
+//        // mapeamento de propriedade aninhada (encadeamento).
+//        dto.setVagaCodigo(clienteVaga.getVaga().getCodigo());
+//        dto.setValor(clienteVaga.getValor());
+//        dto.setDesconto(clienteVaga.getDesconto());
+//
+//        return dto;
+//    }
 }
