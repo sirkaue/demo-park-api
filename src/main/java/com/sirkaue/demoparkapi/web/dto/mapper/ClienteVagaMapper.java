6200ba989cc6be9dc6e12fd29bb99f60e9628f52
sirkaue <kauekdslopes@gmail.com>
2024-08-16 22:49:23 -0300

    entendendo como se faz ClienteVagaMapper manual

--- src/main/java/com/sirkaue/demoparkapi/web/dto/mapper/ClienteVagaMapper.java
+++ src/main/java/com/sirkaue/demoparkapi/web/dto/mapper/ClienteVagaMapper.java
@@ -1,5 +1,7 @@
 package com.sirkaue.demoparkapi.web.dto.mapper;
 
+//import com.sirkaue.demoparkapi.entity.Cliente;
+
 import com.sirkaue.demoparkapi.entity.ClienteVaga;
 import com.sirkaue.demoparkapi.web.dto.EstacionamentoCreateDto;
 import com.sirkaue.demoparkapi.web.dto.EstacionamentoResponseDto;
@@ -13,6 +15,16 @@ public class ClienteVagaMapper {
     public static ClienteVaga toClienteVaga(EstacionamentoCreateDto dto) {
         return new ModelMapper().map(dto, ClienteVaga.class);
     }
+
+// Forma manual de se fazer 'mapeamento de propriedade aninhada (encadeamento)'.
+//    public static ClienteVaga toClienteVaga(EstacionamentoCreateDto dto) {
+//        ClienteVaga clienteVaga = new ClienteVaga();
+//        Cliente cliente = new Cliente();
+//
+//        cliente.setCpf(dto.getClienteCpf());
+//        clienteVaga.setCliente(cliente);
+//        return clienteVaga;
+//    }
 
     public static EstacionamentoResponseDto toDto(ClienteVaga clienteVaga) {
         return new ModelMapper().map(clienteVaga, EstacionamentoResponseDto.class);
