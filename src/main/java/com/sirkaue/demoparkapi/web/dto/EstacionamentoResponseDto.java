938ba7bc9b43163ec45018aad28d02b3374a4465
sirkaue <kauekdslopes@gmail.com>
2024-08-17 19:56:42 -0300

    Melhorando ResponseDto

--- src/main/java/com/sirkaue/demoparkapi/web/dto/EstacionamentoResponseDto.java
+++ src/main/java/com/sirkaue/demoparkapi/web/dto/EstacionamentoResponseDto.java
@@ -1,8 +1,12 @@
 package com.sirkaue.demoparkapi.web.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class EstacionamentoResponseDto {
 
     private String placa;
@@ -11,7 +15,11 @@ public class EstacionamentoResponseDto {
     private String cor;
     private String clienteCpf;
     private String recibo;
+
+    @JsonFormat(pattern = "yyyy-MM-dd hh:mm:ss")
     private LocalDateTime dataEntrada;
+
+    @JsonFormat(pattern = "yyyy-MM-dd hh:mm:ss")
     private LocalDateTime dataSaida;
     private String vagaCodigo;
     private BigDecimal valor;
