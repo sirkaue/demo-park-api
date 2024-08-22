f327eec767d194532f49eb5593730878ec341e3f
sirkaue <kauekdslopes@gmail.com>
2024-08-22 17:42:31 -0300

    Calculando valor do estacionamento e do desconto

--- src/main/java/com/sirkaue/demoparkapi/util/EstacionamentoUtils.java
+++ src/main/java/com/sirkaue/demoparkapi/util/EstacionamentoUtils.java
@@ -1,8 +1,16 @@
 package com.sirkaue.demoparkapi.util;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 
 public class EstacionamentoUtils {
+
+    private static final double PRIMEIROS_15_MINUTES = 5.00;
+    private static final double PRIMEIROS_60_MINUTES = 9.25;
+    private static final double ADICIONAL_15_MINUTES = 1.75;
+    private static final double DESCONTO_PERCENTUAL = 0.30;
 
     private EstacionamentoUtils() {
     }
@@ -14,5 +22,33 @@ public class EstacionamentoUtils {
                 .replace("-", "")
                 .replace(":", "")
                 .replace("T", "-");
+    }
+
+    public static BigDecimal calcularCusto(LocalDateTime entrada, LocalDateTime saida) {
+        long minutes = entrada.until(saida, ChronoUnit.MINUTES);
+        double total = 0.0;
+
+        if (minutes <= 15) {
+            total = PRIMEIROS_15_MINUTES;
+        } else if (minutes <= 60) {
+            total = PRIMEIROS_60_MINUTES;
+        } else {
+            long addicionalMinutes = minutes - 60;
+            Double totalParts = ((double) addicionalMinutes / 15);
+            if (totalParts > totalParts.intValue()) {
+                total += PRIMEIROS_60_MINUTES + (ADICIONAL_15_MINUTES * (totalParts.intValue() + 1));
+            } else {
+                total += PRIMEIROS_60_MINUTES + (ADICIONAL_15_MINUTES * totalParts.intValue());
+            }
+        }
+
+        return new BigDecimal(total).setScale(2, RoundingMode.HALF_EVEN);
+    }
+
+    public static BigDecimal calcularDesconto(BigDecimal custo, long numeroDeVezes) {
+        BigDecimal desconto = ((numeroDeVezes > 0) && (numeroDeVezes % 10 == 0))
+                ? custo.multiply(new BigDecimal(DESCONTO_PERCENTUAL))
+                : new BigDecimal(0);
+        return desconto.setScale(2, RoundingMode.HALF_EVEN);
     }
 }
