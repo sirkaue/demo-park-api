aa443c0e38e6d0331425565e38feed5de30fb632
sirkaue <kauekdslopes@gmail.com>
2024-09-06 18:06:45 -0300

    Adicionando o recurso no controller para consumir o relatório por cpf

--- src/main/java/com/sirkaue/demoparkapi/web/controller/EstacionamentoController.java
+++ src/main/java/com/sirkaue/demoparkapi/web/controller/EstacionamentoController.java
@@ -3,8 +3,10 @@ package com.sirkaue.demoparkapi.web.controller;
 import com.sirkaue.demoparkapi.entity.ClienteVaga;
 import com.sirkaue.demoparkapi.jwt.JwtUserDetails;
 import com.sirkaue.demoparkapi.repository.projection.ClienteVagaProjection;
+import com.sirkaue.demoparkapi.service.ClienteService;
 import com.sirkaue.demoparkapi.service.ClienteVagaService;
 import com.sirkaue.demoparkapi.service.EstacionamentoService;
+import com.sirkaue.demoparkapi.service.JasperService;
 import com.sirkaue.demoparkapi.web.dto.EstacionamentoCreateDto;
 import com.sirkaue.demoparkapi.web.dto.EstacionamentoResponseDto;
 import com.sirkaue.demoparkapi.web.dto.PageableDto;
@@ -20,18 +22,21 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.io.IOException;
 import java.net.URI;
 
 import static io.swagger.v3.oas.annotations.enums.ParameterIn.PATH;
@@ -43,12 +48,16 @@ import static io.swagger.v3.oas.annotations.enums.ParameterIn.QUERY;
 public class EstacionamentoController {
 
     private final EstacionamentoService estacionamentoService;
-
     private final ClienteVagaService clienteVagaService;
+    private final ClienteService clienteService;
+    private final JasperService jasperService;
 
-    public EstacionamentoController(EstacionamentoService estacionamentoService, ClienteVagaService clienteVagaService) {
+    public EstacionamentoController(EstacionamentoService estacionamentoService, ClienteVagaService clienteVagaService,
+                                    ClienteService clienteService, JasperService jasperService) {
         this.estacionamentoService = estacionamentoService;
         this.clienteVagaService = clienteVagaService;
+        this.clienteService = clienteService;
+        this.jasperService = jasperService;
     }
 
     @Operation(summary = "Operação de check-in", description = "Recurso para dar entrada de um veículo no estacionamento." +
@@ -234,5 +243,20 @@ public class EstacionamentoController {
                     direction = Sort.Direction.ASC) Pageable pageable) {
         Page<ClienteVagaProjection> projection = clienteVagaService.buscarTodosPorUsuarioId(user.getId(), pageable);
         return ResponseEntity.ok(PageableMapper.toDto(projection));
+    }
+
+    @GetMapping("/relatorios")
+    @PreAuthorize("hasRole('CLIENTE')")
+    public ResponseEntity<Void> getRelatorio(HttpServletResponse response,
+                                             @AuthenticationPrincipal JwtUserDetails user) throws IOException {
+        String cpf = clienteService.buscarPorUsuarioId(user.getId()).getCpf();
+        jasperService.addParams("CPF", cpf);
+
+        byte[] bytes = jasperService.gerarPdf();
+
+        response.setContentType(MediaType.APPLICATION_PDF_VALUE);
+        response.setHeader("Content-Disposition", "inline; filename=" + System.currentTimeMillis() + ".pdf");
+        response.getOutputStream().write(bytes);
+        return ResponseEntity.ok().build();
     }
 }
